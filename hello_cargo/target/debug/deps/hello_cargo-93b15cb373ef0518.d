08666f9b48207118bc384dc990984ce73f158eaf
hamadatomoki <hamada.tomoki01@gmail.com>
2022-08-18 13:46:50 +0900

    :books: Learn cargo build system.

---
+++ hello_cargo/target/debug/deps/hello_cargo-93b15cb373ef0518.d
@@ -0,0 +1,5 @@
+/Users/hamadachiki/repos/rust-projects/hello_cargo/target/debug/deps/hello_cargo-93b15cb373ef0518.rmeta: src/main.rs
+
+/Users/hamadachiki/repos/rust-projects/hello_cargo/target/debug/deps/hello_cargo-93b15cb373ef0518.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/hamadachiki/repos/rust-projects/hello_cargo/target/debug/hello_cargo: /Users/hamadachiki/repos/rust-projects/hello_cargo/src/main.rs
