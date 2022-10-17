c3b35fd3916810d4e82f94df2531568a71bb1f11
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-10-17 12:42:50 +0900

    :books: Learn scraper in rust

--- web-scraping/web-scraper/src/main.rs
+++ web-scraping/web-scraper/src/main.rs
@@ -3,5 +3,8 @@ fn main() {
     let response = reqwest::blocking::get(url).unwrap().text().unwrap();
     let document = scraper::Html::parse_document(&response);
     let title_selector = scraper::Selector::parse("h3.lister-item-header>a").unwrap();
-    let titles = document.select(&title_selector).map(|x| x.inner_html())
+    let titles = document.select(&title_selector).map(|x| x.inner_html());
+    titles
+        .zip(1..101)
+        .for_each(|(item, number)| println!("{}. {}", number, item));
 }
