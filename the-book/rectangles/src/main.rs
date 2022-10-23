d6eb239db9017faf91fd4bf6397317a4a54b363c
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-10-23 15:40:29 +0900

    :books: Learn struct in rust.

---
+++ the-book/rectangles/src/main.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangles is {} square pixels.",
+        area(&rect1),
+    );
+    println!("react is {:?}", rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
