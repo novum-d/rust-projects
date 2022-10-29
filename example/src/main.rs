ecd01c1fcc34ded1d24be77d6878ef6bbd9d49c0
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-10-29 12:27:38 +0900

    :bug: Fix a bug.

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,9 +2,9 @@ use std::ops::RangeInclusive;
 
 fn main() {
     let some_u8_value: Option<u8> = Some(0u8); // u8: 0 ~ 255(u8::MIN ~ u8::MAX)
-    let range: RangeInclusive = u8::MIN..=u8::MAX;
+    let _range: RangeInclusive<u8> = u8::MIN..=u8::MAX; // u8::MIN..(u8::MAX + 1)
     match some_u8_value {
-        Some(u8::MIN..=u8::MAX) => println!("some"),
+        Some(_range) => println!("some"),
         None => println!("none"),
     }
 }
