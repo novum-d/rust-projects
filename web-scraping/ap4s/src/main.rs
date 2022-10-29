d5b966c418064b20af0dfa8ad863bda76a95f958
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-10-29 22:31:52 +0900

    :books: Learn web scraper in rust

--- web-scraping/ap4s/src/main.rs
+++ web-scraping/ap4s/src/main.rs
@@ -1,41 +1,23 @@
+use reqwest::Url;
+use url::ParseError as UrlParseError;
+
 fn main() {
     let url = "https://www.google.com/search?q=hoge&oq=hoge";
     let response = reqwest::blocking::get(url)
         .unwrap()
-        .text()
+        .text_with_charset("utf-8")
         .unwrap()
         .replace('\\', "");
-    let html = r#"
-        <!doctype html>
-    <html lang="ja">
-    <head>
-      <meta charset="UTF-8">
-      <title>HTML Sample</title>
-      <link rel="stylesheet" href="style.css">
-      <script type="text/javascript" src="sample.js"></script>
-    </head>
-    <body>
-      <div class="header">Header領域</div>
-      <div class="main">
-        <h1>見出し</h1>
-        <p>コンテンツ</p>
-        <img src="img/sample1.jpg">
-      </div>
-      <div class="yuRUbf">
-        <span>Footer領域</span>
-        <a href="https://google.com"><div>リンク</div></a>
-      </div>
-    </body>
-    </html>
-        "#;
-    // println!("{:?}", &response);
     let document = scraper::Html::parse_document(&response);
-    let title_selector = scraper::Selector::parse("a").unwrap();
-    let titles = document
-        .select(&title_selector)
-        .map(|x| x.value().attr("href").unwrap())
-        .filter(|x| x.contains("/url?q="))
-        .map(|x| x.replace("/url?q=", ""));
+    let title_selector = scraper::Selector::parse(".yuRUbf a").unwrap();
+    let titles = document.select(&title_selector).map(|x| {
+        let href = x.value().attr("href").unwrap();
+        if let Err(UrlParseError::RelativeUrlWithoutBase) = Url::parse(href) {
+            href.replace("/url?q=", "")
+        } else {
+            String::from("")
+        }
+    });
     titles
         .zip(1..101)
         .for_each(|(item, number)| println!("{}. {}", number, item));
