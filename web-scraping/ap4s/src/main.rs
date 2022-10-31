752e68b1271427e74aa675a9eead9ec42e323bde
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-10-31 19:59:17 +0900

    :bug: Fix a url decoding bug.

--- web-scraping/ap4s/src/main.rs
+++ web-scraping/ap4s/src/main.rs
@@ -1,23 +1,32 @@
-use reqwest::Url;
-use url::ParseError as UrlParseError;
+use regex::Regex;
+use urlencoding::decode;
 
 fn main() {
-    let url = "https://www.google.com/search?q=hoge&oq=hoge";
-    let response = reqwest::blocking::get(url)
-        .unwrap()
-        .text_with_charset("utf-8")
-        .unwrap()
-        .replace('\\', "");
+    let url =
+        "https://www.google.com/search?q=AIにおける過学習の説明として、最も適切なものはどれか。";
+    let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+    // print!("{}", response);
     let document = scraper::Html::parse_document(&response);
-    let title_selector = scraper::Selector::parse(".yuRUbf a").unwrap();
-    let titles = document.select(&title_selector).map(|x| {
-        let href = x.value().attr("href").unwrap();
-        if let Err(UrlParseError::RelativeUrlWithoutBase) = Url::parse(href) {
-            href.replace("/url?q=", "")
-        } else {
-            String::from("")
-        }
-    });
+    let title_selector = scraper::Selector::parse("a").unwrap();
+    let rx = Regex::new(r"/url\?q=https://(|www)\...-siken\.com.*").unwrap();
+    println!("{}", rx.is_match("/url?q=https://www.ap-siken.com/kakomon/04_aki/q4.html&amp;sa=U&amp;ved=2ahUKEwiVguawpYr7AhWKgVYBHb0BDdIQFnoECAsQAg&amp;usg=AOvVaw10iBt-XJl_Y-W5i7OzurTr"));
+    let titles = document
+        .select(&title_selector)
+        .map(|x| {
+            let href = decode(x.value().attr("href").unwrap())
+                .unwrap()
+                .into_owned();
+            if rx.is_match(&href) && href.contains("&sa=") {
+                href.replace("/url?q=", "")
+                    .split_once("&sa=")
+                    .unwrap()
+                    .0
+                    .to_string()
+            } else {
+                String::from("")
+            }
+        })
+        .filter(|x| !x.is_empty());
     titles
         .zip(1..101)
         .for_each(|(item, number)| println!("{}. {}", number, item));
