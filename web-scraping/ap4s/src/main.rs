0299714e3338e6e3040ab2548cf72dac0ca4f078
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-11-03 22:53:22 +0900

    :books: Learn module privacy and paths in rust.

--- web-scraping/ap4s/src/main.rs
+++ web-scraping/ap4s/src/main.rs
@@ -1,16 +1,16 @@
 use regex::Regex;
 use urlencoding::decode;
 
-fn main() {
+fn main() {}
+
+fn search_questions(url: &str) ->  {
     let url =
         "https://www.google.com/search?q=AIにおける過学習の説明として、最も適切なものはどれか。";
     let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-    // print!("{}", response);
     let document = scraper::Html::parse_document(&response);
     let title_selector = scraper::Selector::parse("a").unwrap();
-    let rx = Regex::new(r"/url\?q=https://(|www)\...-siken\.com.*").unwrap();
-    println!("{}", rx.is_match("/url?q=https://www.ap-siken.com/kakomon/04_aki/q4.html&amp;sa=U&amp;ved=2ahUKEwiVguawpYr7AhWKgVYBHb0BDdIQFnoECAsQAg&amp;usg=AOvVaw10iBt-XJl_Y-W5i7OzurTr"));
-    let titles = document
+    let rx = Regex::new(r"/url\?q=https?://(|www)\...-siken\.com.*").unwrap();
+    document
         .select(&title_selector)
         .map(|x| {
             let href = decode(x.value().attr("href").unwrap())
@@ -26,8 +26,5 @@ fn main() {
                 String::from("")
             }
         })
-        .filter(|x| !x.is_empty());
-    titles
-        .zip(1..101)
-        .for_each(|(item, number)| println!("{}. {}", number, item));
+        .filter(|x| !x.is_empty()).collect()
 }
