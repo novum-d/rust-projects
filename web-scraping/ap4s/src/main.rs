489e5ed01a6fb90bbeab4a70e6ad868985f36b41
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-11-08 07:59:41 +0900

    :recycle: Refactor code.

--- web-scraping/ap4s/src/main.rs
+++ web-scraping/ap4s/src/main.rs
@@ -4,6 +4,7 @@ use headless_chrome::{Browser, LaunchOptionsBuilder, Tab};
 use regex::Regex;
 use urlencoding::decode;
 
+#[allow(unused_must_use)]
 fn main() {
     // let url =
     //     "https://www.google.com/search?q=AIにおける過学習の説明として、最も適切なものはどれか。";
@@ -20,7 +21,7 @@ fn main() {
     let browser = Browser::new(options).unwrap();
     let tab = browser.wait_for_initial_tab().unwrap();
 
-    // navigate to google form site
+    // navigate to google form website
     tab.navigate_to("https://docs.google.com/forms/d/e/1FAIpQLScRkNwFH-sXyyPK-pYyP8pfCpXo5-I1JyNzB0wo1F_9RXUoJQ/viewform");
     tab.wait_until_navigated();
 
@@ -35,18 +36,39 @@ fn main() {
     tab.find_element_by_xpath(r#"//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span"#)
         .unwrap()
         .click();
-    let png = tab
-        .capture_screenshot(
-            headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Png,
-            None,
-            None,
-            true,
-        )
-        .unwrap();
-    std::fs::write("./page.png", png).unwrap();
+    search_questions(
+        "https://www.google.com/search?q=AIにおける過学習の説明として、最も適切なものはどれか。",
+    );
+    // let png = tab
+    //     .capture_screenshot(
+    //         headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Png,
+    //         None,
+    //         None,
+    //         true,
+    //     )
+    //     .unwrap();
+    // std::fs::write("./page.png", png).unwrap();
 }
 
+#[allow(unused_must_use)]
 fn search_questions(url: &str) -> Vec<String> {
+    let browser = Browser::default().unwrap();
+    let tab = browser.wait_for_initial_tab().unwrap();
+
+    // navigate to past tests website
+    tab.navigate_to(url);
+    tab.wait_until_navigated();
+
+    let links = tab.find_elements("a").unwrap();
+    let rx = Regex::new(r"/url\?q=https?://(|www)\...-siken\.com.*").unwrap();
+
+    println!("----------------------------------------------------------");
+    links.iter().map(|x| {
+        println!("{:?}", x);
+        ""
+    });
+
+    // "//*[@id="rso"]/div[1]/div/div/div[1]/div/div/div[1]/div/a"
     let response = reqwest::blocking::get(url).unwrap().text().unwrap();
     let document = scraper::Html::parse_document(&response);
     let title_selector = scraper::Selector::parse("a").unwrap();
@@ -71,60 +93,81 @@ fn search_questions(url: &str) -> Vec<String> {
         .collect()
 }
 
+#[allow(unused_must_use)]
 fn enter_student_info(tab: &Arc<Tab>, student: &Student) {
-    tab.find_element_by_xpath(&FormXPath::ClassId.value())
+    tab.find_element_by_xpath(&xpath::Form::ClassId.value())
         .unwrap()
         .type_into(&student.class_id);
-    tab.find_element_by_xpath(&FormXPath::Name.value())
+    tab.find_element_by_xpath(&xpath::Form::Name.value())
         .unwrap()
         .type_into(&student.id);
-    tab.find_element_by_xpath(&FormXPath::Id.value())
+    tab.find_element_by_xpath(&xpath::Form::Id.value())
         .unwrap()
         .type_into(&student.name);
+}
+
+#[allow(unused_must_use)]
+fn enter_answear(tab: &Arc<Tab>) {
+    let answers = vec![
+        tab.find_elements_by_xpath(&xpath::Answear::A.value())
+            .unwrap(),
+        tab.find_elements_by_xpath(&xpath::Answear::I.value())
+            .unwrap(),
+        tab.find_elements_by_xpath(&xpath::Answear::U.value())
+            .unwrap(),
+        tab.find_elements_by_xpath(&xpath::Answear::E.value())
+            .unwrap(),
+    ];
+}
+
+mod xpath {
+
+    pub enum Form {
+        Id,
+        Name,
+        ClassId,
+    }
+
+    impl Form {
+        pub fn value(&self) -> String {
+            let xpath = |x: i64| {
+                format!("//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[{}]/div/div/div[2]/div/div[1]/div/div[1]/input", x)
+            };
+            match *self {
+                Form::Id => xpath(3),
+                Form::Name => xpath(2),
+                Form::ClassId => xpath(1),
+            }
+        }
+    }
+
+    pub enum Answear {
+        A,
+        I,
+        U,
+        E,
+    }
+
+    impl Answear {
+        pub fn value(&self) -> String {
+            let xpath = |x: i64| {
+                format!(
+                    r#"//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[{}]/label/div/div[2]/div/span"#,
+                    x
+                )
+            };
+            match *self {
+                Answear::A => xpath(1),
+                Answear::I => xpath(2),
+                Answear::U => xpath(3),
+                Answear::E => xpath(4),
+            }
+        }
+    }
 }
 
 struct Student {
     id: String,
     name: String,
     class_id: String,
-}
-
-enum FormXPath {
-    Id,
-    Name,
-    ClassId,
-}
-
-impl FormXPath {
-    fn value(&self) -> String {
-        let xpath = |x: i64| {
-            format!("//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[{}]/div/div/div[2]/div/div[1]/div/div[1]/input", x)
-        };
-        match *self {
-            FormXPath::Id => xpath(3),
-            FormXPath::Name => xpath(2),
-            FormXPath::ClassId => xpath(1),
-        }
-    }
-}
-
-enum Answear {
-    A,
-    I,
-    U,
-    E,
-}
-
-impl Answear {
-    fn value(&self) -> String {
-        let xpath = |x: i64| {
-            format!("//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[{}]/div/div/div[2]/div/div[1]/div/div[1]/input", x)
-        };
-        match *self {
-            Answear::A => xpath(3),
-            Answear::I => xpath(3),
-            Answear::U => xpath(3),
-            Answear::E => xpath(3),
-        }
-    }
 }
