9e4c89851dd44c4b599b8977129d39a617238c40
HamadaTomoki <hamada.tomoki01@gmail.com>
2022-11-10 23:55:00 +0900

    :recycle: Refactor code.

--- web-scraping/ap4s/src/main.rs
+++ web-scraping/ap4s/src/main.rs
@@ -1,85 +1,114 @@
-use std::sync::Arc;
+use std::{
+    fmt::{self},
+    sync::Arc,
+};
 
-use headless_chrome::{Browser, LaunchOptionsBuilder, Tab};
+use headless_chrome::{Browser, Element, Tab};
 use regex::Regex;
+
+use crate::xpath::{google_form, siken_dot_com};
 
 #[allow(unused_must_use)]
 fn main() {
-    // let url =
-    //     "https://www.google.com/search?q=AIにおける過学習の説明として、最も適切なものはどれか。";
-    // println!("{:#?}", search_questions(url));
-    // let result = search_questions(url);
     let class_id = String::from("IH14A219");
     let id = String::from("90223");
     let name = String::from("浜田知季");
 
+    let form_url = "https://docs.google.com/forms/d/e/1FAIpQLScRkNwFH-sXyyPK-pYyP8pfCpXo5-I1JyNzB0wo1F_9RXUoJQ/viewform";
+
     let browser = Browser::default().unwrap();
-    let tab = browser.wait_for_initial_tab().unwrap();
+    let form_tab = browser.wait_for_initial_tab().unwrap();
 
     // navigate to google form website
-    tab.navigate_to("https://docs.google.com/forms/d/e/1FAIpQLScRkNwFH-sXyyPK-pYyP8pfCpXo5-I1JyNzB0wo1F_9RXUoJQ/viewform");
-    tab.wait_until_navigated();
+    form_tab.navigate_to(form_url);
+    form_tab.wait_until_navigated();
 
     // enter the student infomation
-    enter_student_info(&tab, &Student { class_id, id, name });
+    enter_student_info(&form_tab, &Student { class_id, id, name });
 
     // enter the collect.
-    enter_answear(&tab);
-
-    tab.find_element_by_xpath(r#"//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[1]/label/div/div[2]/div/span"#)
-        .unwrap()
-        .click();
+    enter_answear(&form_tab);
 
     // submit
-    tab.find_element_by_xpath(xpath::SUBMIT).unwrap().click();
-
-    find_website_links(
-        "https://www.google.com/search?q=AIにおける過学習の説明として、最も適切なものはどれか。",
-    );
+    // tab.find_element_by_xpath(xpath::SUBMIT).unwrap().click();
 }
 
 #[allow(unused_must_use)]
-fn enter_student_info(tab: &Arc<Tab>, student: &Student) {
-    tab.find_element_by_xpath(&xpath::Student::ClassId.value())
+fn enter_student_info(form_tab: &Arc<Tab>, student: &Student) {
+    form_tab
+        .find_element_by_xpath(&google_form::Student::ClassId.to_string())
         .unwrap()
         .type_into(&student.class_id);
-    tab.find_element_by_xpath(&xpath::Student::Name.value())
+    form_tab
+        .find_element_by_xpath(&google_form::Student::Name.to_string())
         .unwrap()
         .type_into(&student.id);
-    tab.find_element_by_xpath(&xpath::Student::Id.value())
+    form_tab
+        .find_element_by_xpath(&google_form::Student::Id.to_string())
         .unwrap()
         .type_into(&student.name);
 }
 
 #[allow(unused_must_use)]
-fn enter_answear(tab: &Arc<Tab>) {
-    let links = get_questions(tab)
-        .iter()
-        .map(|x| find_website_links(&format!("https://www.google.com/search?q={}", x)))
-        .collect::<Vec<Vec<String>>>();
+fn enter_answear(form_tab: &Arc<Tab>) {
+    get_answears(form_tab);
     let answers = vec![
-        tab.find_elements_by_xpath(&xpath::Answear::A.value())
+        form_tab
+            .find_elements_by_xpath(&google_form::Option::A.to_string())
             .unwrap(),
-        tab.find_elements_by_xpath(&xpath::Answear::I.value())
+        form_tab
+            .find_elements_by_xpath(&google_form::Option::I.to_string())
             .unwrap(),
-        tab.find_elements_by_xpath(&xpath::Answear::U.value())
+        form_tab
+            .find_elements_by_xpath(&google_form::Option::U.to_string())
             .unwrap(),
-        tab.find_elements_by_xpath(&xpath::Answear::E.value())
+        form_tab
+            .find_elements_by_xpath(&google_form::Option::E.to_string())
             .unwrap(),
     ];
 }
 
 #[allow(unused_must_use)]
+fn get_answears(form_tab: &Arc<Tab>) {
+    let links = get_questions(form_tab)
+        .iter()
+        .map(|title| find_website_links(&Url::GoogleSearch(title).to_string()))
+        .collect::<Vec<Vec<String>>>();
+
+    'top: for link in links.iter() {
+        let browser = Browser::default().unwrap();
+        let siken_tab = browser.wait_for_initial_tab().unwrap();
+        for url in link.iter() {
+            println!("{}", url);
+            siken_tab.navigate_to(url);
+            siken_tab.wait_until_navigated();
+            let answear = get_node_value(
+                &siken_tab
+                    .find_element_by_xpath(&siken_dot_com::Question::Answear.to_string())
+                    .unwrap(),
+            );
+
+            let collect = get_node_value(
+                &siken_tab
+                    .find_element_by_xpath(&siken_dot_com::Question::Collect.to_string())
+                    .unwrap(),
+            );
+            println!("{}: {}", answear, collect)
+        }
+    }
+}
+
+#[allow(unused_must_use)]
 fn find_website_links(url: &str) -> Vec<String> {
     let browser = Browser::default().unwrap();
-    let tab = browser.wait_for_initial_tab().unwrap();
+    let browser_tab = browser.wait_for_initial_tab().unwrap();
 
     // navigate to past tests website
-    tab.navigate_to(url);
-    tab.wait_until_navigated();
+    browser_tab.navigate_to(url);
+    browser_tab.wait_until_navigated();
 
     let rx = Regex::new(r#"^https?://(|www)\...-siken\.com.*$"#).unwrap();
-    let els = tab.find_elements("a").unwrap();
+    let els = browser_tab.find_elements("a").unwrap();
     let attrs = els.iter().map(|x| x.get_attributes().unwrap().unwrap());
     let mut urls = vec![];
     attrs.for_each(|x| {
@@ -93,61 +122,89 @@ fn find_website_links(url: &str) -> Vec<String> {
 }
 
 #[allow(unused_must_use)]
-fn get_questions(tab: &Arc<Tab>) -> Vec<String> {
-    tab.find_elements_by_xpath(r#"//*[@class="M7eMe"]/span"#)
+fn get_questions(form_tab: &Arc<Tab>) -> Vec<String> {
+    form_tab
+        .find_elements_by_xpath(r#"//*[@class="M7eMe"]/span"#)
         .unwrap()
         .iter()
-        .map(|x| {
-            x.get_description().unwrap().children.unwrap()[0]
-                .node_value
-                .clone()
-        })
+        .map(get_node_value)
         .collect()
+}
+
+fn get_node_value(el: &Element) -> String {
+    el.get_description().unwrap().children.unwrap()[0]
+        .node_value
+        .clone()
 }
 
 mod xpath {
 
-    pub const submit: &str = r#"//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span"#;
+    pub mod siken_dot_com {
+        use std::fmt;
 
-    pub enum Student {
-        Id,
-        Name,
-        ClassId,
-    }
-
-    impl Student {
-        pub fn value(&self) -> String {
-            let xpath = |x: i64| {
-                format!("//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[{}]/div/div/div[2]/div/div[1]/div/div[1]/input", x)
-            };
-            match *self {
-                Student::Id => xpath(3),
-                Student::Name => xpath(2),
-                Student::ClassId => xpath(1),
+        pub enum Question {
+            Answear,
+            Collect,
+        }
+        impl fmt::Display for Question {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(
+                    f,
+                    "{}",
+                    match *self {
+                        Self::Answear =>
+                            r#"//*[@id="mainCol"]/div[2]/div[2] | //*[@id="mainCol"]/div[2]/section | //*[@id="mainCol"]/div[2]/p[1]"#,
+                        Self::Collect => r#"//*[@id="answerChar"]"#,
+                    }
+                )
             }
         }
     }
 
-    pub enum Answear {
-        A,
-        I,
-        U,
-        E,
-    }
+    pub mod google_form {
+        use std::fmt;
 
-    impl Answear {
-        pub fn value(&self) -> String {
-            let xpath = |x: i64| {
-                format!(
-                    r#"//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[{}]/label/div/div[2]/div/span"#,
-                    x
+        pub const SUBMIT: &str = r#"//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span"#;
+
+        pub enum Student {
+            Id,
+            Name,
+            ClassId,
+        }
+
+        impl fmt::Display for Student {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(
+                    f,
+                    r#"//*[@id="mG61Hd"]/div[2]/div/div[2]/div[{}]/div/div/div[2]/div/div[1]/div/div[1]/input"#,
+                    match &self {
+                        Self::Id => 3,
+                        Self::Name => 2,
+                        Self::ClassId => 1,
+                    }
                 )
-            };
-            match *self {
-                Answear::A => xpath(1),
-                Answear::I => xpath(2),
-                Answear::U => xpath(3),
-                Answear::E => xpath(4),
+            }
+        }
+
+        pub enum Option {
+            A,
+            I,
+            U,
+            E,
+        }
+
+        impl fmt::Display for Option {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(
+                    f,
+                    r#"//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[{}]/label/div/div[2]/div/span"#,
+                    match *self {
+                        Self::A => 1,
+                        Self::I => 2,
+                        Self::U => 3,
+                        Self::E => 4,
+                    }
+                )
             }
         }
     }
@@ -159,15 +216,14 @@ struct Student {
     class_id: String,
 }
 
-pub enum Url {
-    GoogleForm(String),
-    GoogleSearch(String),
+pub enum Url<'a> {
+    GoogleSearch(&'a str),
 }
 
-impl Url {
-    pub fn value(&self) -> String {
+impl fmt::Display for Url<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
-            Url::GoogleSearch => |title| format!("https://www.google.com/search?q={}", title),
+            Url::GoogleSearch(tltle) => write!(f, "https://www.google.com/search?q={}", tltle),
         }
     }
 }
