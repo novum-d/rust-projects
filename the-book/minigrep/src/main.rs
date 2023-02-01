f10c932958380f65da58253001b19736f446af47
hamadatomoki <hamada.tomoki01@gmail.com>
2023-02-01 20:21:59 +0900

    :books: learn: update return to  Resut<(),Box<dynError>>

--- the-book/minigrep/src/main.rs
+++ the-book/minigrep/src/main.rs
@@ -1,9 +1,25 @@
-use core::panic;
-use std::env;
+use std::{env, error::Error, fs::File, io::Read, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    run(config);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
