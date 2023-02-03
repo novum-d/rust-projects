5b56db20f5ce12471f254a0131cd10ca121f7d9f
hamadatomoki <hamada.tomoki01@gmail.com>
2023-02-03 16:28:07 +0900

    :books: learn: simulate TDD in rust

--- the-book/minigrep/src/lib.rs
+++ the-book/minigrep/src/lib.rs
@@ -10,9 +10,9 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough argments");
         }
-
         let query = args[1].clone();
         let filename = args[2].clone();
+
         Ok(Config { query, filename })
     }
 }
@@ -21,7 +21,34 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(config.filename)?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line)
+    }
 
-    println!("With text:\n{}", contents);
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
