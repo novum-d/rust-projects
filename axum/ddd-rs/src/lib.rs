5df12369cf8f4c210e6f76624f84843c5d372f52
hamadatomoki <hamada.tomoki01@gmail.com>
2023-02-11 01:40:35 +0900

    :building_construction: chore: update db connection test.

--- axum/ddd-rs/src/lib.rs
+++ axum/ddd-rs/src/lib.rs
@@ -1,42 +1,33 @@
 #[macro_use]
 extern crate dotenv_codegen;
 
-use std::any;
-
-use log::info;
-use sqlx::{postgres::PgRow, PgPool, Postgres, Row};
-
-struct Stores {
-    city: String,
-    hours: String,
-    name: String,
-    open: bool,
-    phone_number: String,
-    street: String,
-    zip: String,
-}
-
-#[sqlx::test]
-async fn it_connects() -> sqlx::Result<()> {
-    let pool = PgPool::connect(dotenv!("DATABASE_URL")).await?;
-
-    let stores = sqlx::query("SELECT * FROM stores")
-        .map(|x: PgRow| {
-            info!("column nu!!!!!!!!!!!!!!!!!!1{}", x.len());
-            x
-        })
-        .fetch_all(&pool)
-        .await?;
-    Ok(())
-}
-
 #[cfg(test)]
 mod test {
+    use sqlx::PgPool;
+
     #[test]
-    fn dotenv_test() {
-        assert_eq!(
-            "postgres://postgres:postgres@localhost/lv_ex_dev",
-            dotenv!("DATABASE_URL")
-        )
+    fn dotenv() {
+        let database_url = "postgres://postgres:postgres@localhost/lv_ex_dev";
+        assert_eq!(database_url, dotenv!("DATABASE_URL"))
+    }
+
+    #[sqlx::test]
+    async fn it_connects() -> sqlx::Result<()> {
+        let pool = PgPool::connect(dotenv!("DATABASE_URL")).await?;
+        sqlx::query_as::<_, Stores>("SELECT * FROM stores")
+            .fetch_all(&pool)
+            .await?;
+        Ok(())
+    }
+
+    #[derive(sqlx::FromRow, Debug)]
+    pub struct Stores {
+        city: String,
+        hours: String,
+        name: String,
+        open: bool,
+        phone_number: String,
+        street: String,
+        zip: String,
     }
 }
