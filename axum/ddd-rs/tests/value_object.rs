8c8a9d6c176d941add92e85188e662ee34083653
hamadatomoki <hamada.tomoki01@gmail.com>
2023-02-14 11:25:40 +0900

    :books: learn: add value object test

---
+++ axum/ddd-rs/tests/value_object.rs
@@ -0,0 +1,21 @@
+#[test]
+fn show_lastname() {
+    // Don't
+    let user_name = "tomoki hamada";
+    let mut splits = user_name.split_whitespace();
+    let last = splits.nth(1).unwrap();
+    assert_eq!(last, "hamada");
+
+    // Do
+    let user_name = UserName {
+        first: "tomoki".to_owned(),
+        last: "hamada".to_owned(),
+    };
+    assert_eq!(user_name.last, "hamada");
+}
+
+#[allow(dead_code)]
+struct UserName {
+    first: String,
+    last: String,
+}
