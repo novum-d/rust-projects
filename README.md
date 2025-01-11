7fa363844ba41f92e596f67f15138d17cf62abee
novum-d <hamada.tomoki01@gmail.com>
2025-01-11 19:08:04 +0900

    :learn: rust clap

---
+++ README.md
@@ -0,0 +1,8 @@
+```sh
+rustup self update #rustup自身のアップデートがないかチェックし、ある場合はアップデート
+rustup update #rustのアップデートがないかチェックし、ある場合はアップデート
+```
+
+```sh
+rustup component add clippy rls rust-analysis rust-src rust-docs rustfmt rust-analyzer
+```
--- cli/echor/src/main.rs
+++ cli/echor/src/main.rs
@@ -1,7 +1,16 @@
-// #[derive(Parser)]
-// #[command(version, about)]
-// struct Cli {}
+use clap::{arg, Command};
 
 fn main() {
-    // println!("{:?}", args())
+    let mathces = Command::new("echor")
+        .version("0.1.0")
+        .author("novumd <novumd@gmail.com>")
+        .about("Rust echo")
+        .args([
+            arg!(-t --text <TEXT> ... "Input text")
+                .required(true)
+                .num_args(1..),
+            arg!(-n --omit_newline "Do not print newline").num_args(0),
+        ])
+        .get_matches();
+    println!("{:#?}", mathces);
 }
