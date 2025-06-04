0f2c0b3a90d44e34b5c323d6d27054bca95f5f2e
novum-d <hamada.tomoki01@gmail.com>
2025-06-05 08:44:36 +0900

    :learn: rust

--- cli/headr/src/lib.rs
+++ cli/headr/src/lib.rs
@@ -1,5 +1,8 @@
 use std::error::Error;
 use std::fmt::format;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
 use clap::{arg, Arg, Command};
 
 pub type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -70,7 +73,17 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in &config.files {
+        match open(filename) {
+            Err(e) => { eprintln!("Failed to open {}: {}", filename, e) }
+            Ok(input) => {
+                for line in input.lines().take(config.lines) {
+                    println!("{}", line?)
+                }
+            }
+        }
+    }
+    dbg!(&config);
     Ok(())
 }
 
@@ -79,5 +92,12 @@ pub fn parse_positive_Int(s: &str) -> MyResult<usize> {
     match s.parse() {
         Ok(n) if n > 0 => Ok(n),
         _ => Err(From::from(s)),
+    }
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
 }
